0853d7f0dd6e165f757f4aef84ce7bda3a88e04d
Jan Boerman <janboerman95@gmail.com>
2022-04-04 12:49:45 +0200

    I value compatibility

--- src/main/java/com/janboerman/jargenerator/JarGeneratorConfig.java
+++ src/main/java/com/janboerman/jargenerator/JarGeneratorConfig.java
@@ -20,6 +20,7 @@ public interface JarGeneratorConfig extends Config {
         return Color.CYAN;
     }
 
+    //haha, this is a spelling mistake, but we're stuck with it now! It's only allowed to be fixed when we release a "2.0" version of this plugin.
     @ConfigItem(keyName = "unkownChargesColour", name = "Unknown colour", description = "Configures the text colour when the amount of charges is not exactly known", position = 2)
     default Color unknownChargesColour() {
         return new Color(0xFF, 0x40, 0x00);
